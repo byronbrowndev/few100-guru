c84f47dd53e38b1ce1d85a29e5966fcad02a163c
Jeffry Gonzalez <jeff@hypertheory.com>
2021-12-13 15:53:12 -0500

    using let

--- src/app/learning/01-basic-types.spec.ts
+++ src/app/learning/01-basic-types.spec.ts
@@ -12,7 +12,41 @@ describe('basic data types', () => {
   describe('declaring variables', () => {
 
     it('untyped variables', () => {
+      let a;
 
+      a = 19;
+
+      expect(a).toBe(19);
+
+      a = 'tacos';
+
+      expect(a).toBe('tacos');
+
+      a = function (x: number) {
+        return x * 2;
+      }
+
+      expect(typeof (a)).toBe('function');
+
+      expect(a(10)).toBe(20);
+    });
+    it('setting data types for a variable', () => {
+
+      let x: number | string; // Union.
+
+      x = 12;
+
+
+      x = 'tacos';
+
+    });
+
+    it('initializing a variable', () => {
+      let x: number | string = 12;
+      let y = 18;
+
+
+      x = 'Tacos';
     });
   });
 });
