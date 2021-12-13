21f00fce393cae3b55cbed85c86145fca320756b
Jeffry Gonzalez <jeff@hypertheory.com>
2021-12-13 15:39:24 -0500

    writing a spec

---
+++ src/app/learning/01-basic-types.spec.ts
@@ -0,0 +1,18 @@
+describe('basic data types', () => {
+
+  it('is easy to declare a variable', () => {
+
+    let a = 10, b = 20;
+
+    let answer = a + b;
+
+    expect(answer).toBe(30);
+  });
+
+  describe('declaring variables', () => {
+
+    it('untyped variables', () => {
+
+    });
+  });
+});
